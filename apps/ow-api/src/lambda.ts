766762a1fbdcc967ee6ce23ffbdd48ad78cf8775
magnusrodseth <59113973+magnusrodseth@users.noreply.github.com>
2022-09-14 19:13:20 +0200

    nuke ow-api

--- apps/ow-api/src/lambda.ts
+++
@@ -1,2 +0,0 @@
-// TODO: Integrate tRPC with AWS lambda: https://trpc.io/docs/v9/aws-lambda#3-use-the-amazon-api-gateway-adapter
-export { }
--- apps/ow-api/src/server.ts
+++
@@ -1,7 +0,0 @@
-import express from "express"
-
-export const createServer = async () => {
-  const app = express()
-  // TODO: Start local server
-  return app
-}
